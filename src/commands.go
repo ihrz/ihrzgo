0571e10bd1e2a83161ea52f95644ea73285cab3b
Anaïs Saraiva <53227802+Kisakay@users.noreply.github.com>
2023-10-26 09:02:25 +0200

    better slash command handler

--- src/commands.go
+++ src/commands.go
@@ -18,9 +18,35 @@ func AddCommand(command *discordgo.ApplicationCommand, handler func(s *discordgo
 // RegisterCommands enregistre toutes les commandes dans Discord
 func RegisterCommands(s *discordgo.Session, botID, guildID string) {
 
+	commands, err := s.ApplicationCommands(botID, "")
+	if err != nil {
+		panic(err)
+	}
+
+	for _, command := range commands {
+		fmt.Println(command.Name)
+
+		// Vérifiez si la commande actuelle doit être supprimée
+		shouldDelete := true
+		for _, registeredCommand := range registeredCommands {
+			if registeredCommand.Name == command.Name {
+				shouldDelete = false
+				break
+			}
+		}
+
+		if shouldDelete {
+			// Supprimez la commande
+			err := s.ApplicationCommandDelete(botID, "", command.ID)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
 	for _, command := range registeredCommands {
 		fmt.Println("Registering :", command.Name)
-		_, err := s.ApplicationCommandCreate(botID, guildID, command)
+		_, err := s.ApplicationCommandCreate(botID, "", command)
 		if err != nil {
 			panic(err)
 		}
